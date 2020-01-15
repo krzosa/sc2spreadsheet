e6c75fee3dbdab5dc97d48044c62989e8df86acc
Krzosa <krzosa.karol@gmail.com>
2020-01-15 20:08:41 +0100

    refactor: most of the functionality in functions

--- sc2Spreadsheet.py
+++ sc2Spreadsheet.py
@@ -1,8 +1,15 @@
 import mpyq
-import pprint #pretty print
 import time
+import subprocess
 from datetime import datetime
 from s2protocol import versions
+
+# TODO: 
+#   loading settings from file
+#   loading files from directory outputing stats
+#   send to spreadsheet
+#   
+
 
 def getGameTime(header):
     # game time from sc2 client / game loops to time
@@ -15,52 +22,64 @@ def getGameTime(header):
     return f'{h:d}:{m:02d}:{s:02d}'
 
 
+# returns the time and date when the game was played
+# TODO: check if this is game end date or not
+def getGameDate(protocolDetails):
+    return datetime.fromtimestamp(round(
+            protocolDetails['m_timeUTC'] / (10 * 1000 * 1000) - 11644473600 - 
+                        ((protocolDetails['m_timeLocalOffset'] / 10000000))))
+
+
+def getPlayerInfo(protocolDetails, protocolInitData, playerCount=2):
+    playerInfo = []
+    for i in range(0, playerCount):
+        playerInfo.append([
+            protocolDetails['m_playerList'][i]['m_name'],
+            protocolDetails['m_playerList'][i]['m_race'],
+            protocolDetails['m_playerList'][i]['m_result'],
+            protocolInitData['m_syncLobbyState']['m_userInitialData'][i]['m_scaledRating'],
+            protocolInitData['m_syncLobbyState']['m_userInitialData'][i]['m_highestLeague']])
+    return playerInfo
+
+
+def getPlayerCount(protocolDetails):
+    return len(protocolDetails['m_playerList'])
+
+
+def getMapName(protocolDetails):
+    return protocolDetails['m_title']
+
+
+def getProtocolDetails(protocol):
+    return protocol.decode_replay_details(
+                archive.read_file('replay.details'))
+
+
+def getProtocolInitData(protocol):
+    return protocol.decode_replay_initdata(
+                archive.read_file('replay.initData'))
+
+
+def getHeaderAndProtocol(archive):
+    contents = archive.header['user_data_header']['content']
+    header = versions.latest().decode_replay_header(contents)
+    protocol = versions.build(header['m_version']['m_baseBuild'])
+    return [header, protocol]
+
+
 archive = mpyq.MPQArchive('a.sc2replay')
 # archive = mpyq.MPQArchive('Efemeryda ER.sc2replay')
-contents = archive.header['user_data_header']['content']
-header = versions.latest().decode_replay_header(contents)
-baseBuild = header['m_version']['m_baseBuild']
-protocol = versions.build(baseBuild)
+header, protocol = getHeaderAndProtocol(archive)
 
-contents = archive.read_file('replay.details')
-protocolDetails = protocol.decode_replay_details(contents)
-contents = archive.read_file('replay.initData')
-protocolInitData = protocol.decode_replay_initdata(contents)
+protocolDetails = getProtocolDetails(protocol)
+protocolInitData = getProtocolInitData(protocol)
 
-gameDetails = []
-playerInfo = []
-gameDetails.append(len(protocolDetails['m_playerList']))
-
-for i in range(0, gameDetails[0]):
-    playerInfo.append([protocolDetails['m_playerList'][i]['m_name'],
-    protocolDetails['m_playerList'][i]['m_race'],
-    protocolDetails['m_playerList'][i]['m_result'],
-    protocolInitData['m_syncLobbyState']['m_userInitialData'][i]['m_scaledRating'],
-    protocolInitData['m_syncLobbyState']['m_userInitialData'][i]['m_highestLeague']])
-
-gameDetails.append(protocolDetails['m_title'])
-
-print(playerInfo, ' ', gameDetails)
-gameDetails.append(protocolDetails['m_timeUTC'] / 
-                  (10 * 1000 * 1000) - 11644473600 - 
-                  ((protocolDetails['m_timeLocalOffset'] / 10000000)))
-print(datetime.fromtimestamp(gameDetails[2]))
-# print(protocolDetails['m_playerList'][0]['m_name'])
-# print(protocolDetails['m_playerList'][0]['m_result'])
-# print(protocolDetails['m_playerList'][0]['m_race'])
-# print(protocolDetails['m_playerList'][1]['m_name'])
-# print(protocolDetails['m_playerList'][1]['m_result'])
-# print(protocolDetails['m_playerList'][1]['m_race'])
-# print(protocolDetails['m_title'])
-# print(protocolDetails['m_timeUTC'])
-# print(protocolDetails['m_timeLocalOffset'])
-
-# pprint.pprint(protocolInitData['m_syncLobbyState']['m_userInitialData'][1]['m_scaledRating'])
-
-
-# contents = archive.read_file('replay.load.info')
-# protocolLoadInfo = protocol.decode_replay_load_info(contents)
-
-
-
+print(getPlayerInfo(protocolDetails, protocolInitData))
+print(getMapName(protocolDetails))
+print(getGameDate(protocolDetails))
 print(getGameTime(header))
+
+
+
+
+
